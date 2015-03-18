344debb076938a7d5217afdef749be763c936d3c
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2015-03-18 13:24:40 +0200

    Adding solution for fibonacci

--- week1/1-Warmups/warmup.py
+++ week1/1-Warmups/warmup.py
@@ -1,3 +1,20 @@
+def fibonacci(n):
+    result = []
+
+    a = 1
+    b = 1
+
+    while len(result) < n:
+        result.append(a)
+
+        next_fib = a + b
+        a = b
+        b = next_fib
+
+
+    return result
+
+
 def factorial(n):
     result = 1
 
