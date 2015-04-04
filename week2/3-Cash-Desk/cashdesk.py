eca86d4fe8d9cbc9b11e5c38d057876190a2e463
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2015-04-04 14:43:33 +0300

    Adding solutions and tests for cashdesk

--- week2/3-Cash-Desk/cashdesk.py
+++ week2/3-Cash-Desk/cashdesk.py
@@ -22,3 +22,60 @@ class Bill:
 
     def __hash__(self):
         return hash(self.__str__())
+
+    # Used for sorting
+    def __lt__(self, other):
+        return int(self) < int(other)
+
+
+class BillBatch:
+    def __init__(self, bills):
+        self.__bills = bills
+
+    def __len__(self):
+        return len(self.__bills)
+
+    def __getitem__(self, index):
+        return self.__bills[index]
+
+    def total(self):
+        return sum([int(bill) for bill in self.__bills])
+
+
+class CashDesk:
+    def __init__(self):
+        self.money_holder = {}
+
+    def __store_money(self, bill):
+        if bill not in self.money_holder:
+            self.money_holder[bill] = 1
+        else:
+            self.money_holder[bill] += 1
+
+    def take_money(self, money):
+        if isinstance(money, Bill):
+            self.__store_money(money)
+        elif isinstance(money, BillBatch):
+            for bill in money:
+                self.__store_money(bill)
+
+    def total(self):
+        m = self.money_holder
+        return sum([int(bill) * m[bill] for bill in m])
+
+    def inspect(self):
+        lines = []
+        total = self.total()
+
+        lines.append("We have {}$ in the desk.".format(total))
+
+        if total > 0:
+            lines.append("Bills are:")
+
+            bills = list(self.money_holder.keys())
+            bills.sort()
+
+            for bill in bills:
+                line = "${} - {}".format(int(bill), self.money_holder[bill])
+                lines.append(line)
+        return "\n".join(lines)
--- week2/3-Cash-Desk/cashdesk_test.py
+++ week2/3-Cash-Desk/cashdesk_test.py
@@ -4,33 +4,30 @@
 # 3. Refactor while testing
 # 4. Loop back to 1)
 import unittest
-from cashdesk import Bill
+from cashdesk import Bill, BillBatch, CashDesk
 
 
 class CashDeskTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_bill = Bill(10)
+
     def test_create_new_bill_class(self):
-        bill = Bill(10)
-        self.assertTrue(isinstance(bill, Bill))
+        self.assertTrue(isinstance(self.test_bill, Bill))
 
     def test_create_int_value_from_bill(self):
-        bill = Bill(10)
-        self.assertEqual(int(bill), 10)
+        self.assertEqual(int(self.test_bill), 10)
 
     def test_amount_in_bill(self):
-        bill = Bill(10)
 
         with self.assertRaises(AttributeError):
-            bill.amount
+            self.test_bill.amount
 
     def test_str_dunder_for_bill(self):
-        bill = Bill(10)
-
-        self.assertEqual(str(bill), "A 10$ bill.")
+        self.assertEqual(str(self.test_bill), "A 10$ bill.")
 
     def test_repr_dunder_for_bill(self):
-        bill = Bill(10)
-
-        self.assertEqual(repr(bill), "A 10$ bill.")
+        self.assertEqual(repr(self.test_bill), "A 10$ bill.")
 
     def test_eq_between_bills_when_not_same(self):
         bill1 = Bill(10)
@@ -44,10 +41,11 @@ class CashDeskTest(unittest.TestCase):
 
         self.assertTrue(bill1 == bill2)
 
-    def test_can_hash_bill(self):
-        bill = Bill(10)
+    def test_bills_are_orderable(self):
+        self.assertTrue(Bill(5) < Bill(10))
 
-        self.assertIsNotNone(hash(bill))
+    def test_can_hash_bill(self):
+        self.assertIsNotNone(hash(self.test_bill))
 
     def test_can_put_bill_in_dictionary(self):
         money_holder = {}
@@ -59,16 +57,77 @@ class CashDeskTest(unittest.TestCase):
 
     def test_value_error_raises_from_negative_amount(self):
         with self.assertRaises(ValueError):
-            bill = Bill(-10)
+            Bill(-10)
 
     def test_value_error_raises_from_zero_amount(self):
         with self.assertRaises(ValueError):
-            bill = Bill(0)
+            Bill(0)
 
     def test_type_error_raises_from_float_amount(self):
         with self.assertRaises(TypeError):
-            bill = Bill(0.5)
+            Bill(0.5)
 
+    def test_can_create_billbatch(self):
+        batch = BillBatch([])
+        self.assertTrue(isinstance(batch, BillBatch))
+
+    def test_can_create_billbatch_with_bills(self):
+        bills = [Bill(value) for value in range(1, 10)]
+        batch = BillBatch(bills)
+
+        self.assertEqual(len(batch), len(range(1, 10)))
+        self.assertEqual(batch.total(), sum(range(10)))
+
+    def test_can_for_loop_a_billbatch(self):
+        bills = [Bill(value) for value in range(1, 10)]
+        batch = BillBatch(bills)
+        total = 0
+
+        for bill in batch:
+            total += int(bill)
+
+        self.assertEqual(total, sum(range(1, 10)))
+
+    def test_can_create_cashdesh(self):
+        desk = CashDesk()
+        self.assertTrue(isinstance(desk, CashDesk))
+
+    def test_cashdesk_take_only_bills(self):
+        desk = CashDesk()
+
+        desk.take_money(Bill(10))
+        desk.take_money(Bill(5))
+
+        self.assertEqual(desk.total(), 15)
+
+    def test_cashdesk_take_only_batch(self):
+        desk = CashDesk()
+        batch = BillBatch([Bill(10), Bill(5)])
+
+        desk.take_money(batch)
+
+        self.assertEqual(desk.total(), 15)
+
+    def test_inspect_on_empty_desk(self):
+        desk = CashDesk()
+        expected = "We have 0$ in the desk."
+
+        self.assertEqual(desk.inspect(), expected)
+
+    def test_inspect_with_full_desk(self):
+        desk = CashDesk()
+
+        desk.take_money(Bill(1))
+        desk.take_money(Bill(2))
+        desk.take_money(Bill(10))
+
+        expected = ["We have 13$ in the desk."]
+        expected.append("Bills are:")
+        expected.append("$1 - 1")
+        expected.append("$2 - 1")
+        expected.append("$10 - 1")
+
+        self.assertEqual("\n".join(expected), desk.inspect())
 
 if __name__ == '__main__':
     unittest.main()
