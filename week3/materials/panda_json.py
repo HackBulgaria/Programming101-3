66428a3d03b3dc815b89d69ae78235943ba07585
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2015-04-04 14:52:25 +0300

    Moving panda_json.py to materials

--- week3/materials/panda_json.py
+++ week3/materials/panda_json.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Panda:
     def __init__(self, name, email, gender):
         self.name = name
@@ -8,9 +9,10 @@ class Panda:
 
     def __str__(self):
         return "{} - {} - {}".format(self.name, self.email, self.gender)
-    
+
     def __repr__(self):
-        return "Panda('{}', '{}', '{}')".format(self.name, self.email, self.gender)
+        return "Panda('{}', '{}', '{}')".format(self.name,
+                                                self.email, self.gender)
 
 
 def serialize_to(path, data):
@@ -19,11 +21,13 @@ def serialize_to(path, data):
     with open(path, "w") as f:
         f.write(json_string)
 
+
 def unserialize_from(path):
     with open(path, "r") as f:
         contents = f.read()
 
     return json.loads(contents)
 
-# print(unserialize_from("data.json"))
-
+p = Panda("Ivo", "ivo@pandamail.com", "male")
+serialize_to("panda.json", repr(p))
+print(unserialize_from("panda.json"))
