c41dfce30a890057e17ed75006f0aa527d8d5e02
RadoRado <radorado@hackbulgaria.com>
2015-04-29 16:04:00 +0300

    Add sqlite.Row example

--- week7/2-SQL-Starter/query_table.py
+++ week7/2-SQL-Starter/query_table.py
@@ -1,16 +1,18 @@
 import sqlite3
 
 db = sqlite3.connect("users.db")
+db.row_factory = sqlite3.Row
+
 cursor = db.cursor()
 
 select_one = """
-SELECT id, email, gender
+SELECT email, id, gender
 FROM users
 WHERE id = 1
 """
 
 cursor_result = cursor.execute(select_one)
-row = cursor_result.fetchone()
 
-print(row)
+row = cursor_result.fetchone()
+print(row["email"])
 
